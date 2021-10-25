928367dd46c496bd99529216431d6f3ac3f45209
mensinkj <joshuamensink@gmail.com>
2021-10-25 17:58:15 -0500

    Section 3 Completion
    Navbar, Models, Component Set up

--- hamburger-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ hamburger-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Joy Love Burger", "The most Delicious burger ever", "https://s3-media0.fl.yelpcdn.com/bphoto/zNYvfhQA1D1m3yKMoM4R0A/o.jpg"),
+    new Recipe("Joy Love Burger", "The most Delicious burger ever", "https://s3-media0.fl.yelpcdn.com/bphoto/zNYvfhQA1D1m3yKMoM4R0A/o.jpg"),
+  ];
+
+
 
   constructor() { }
 
